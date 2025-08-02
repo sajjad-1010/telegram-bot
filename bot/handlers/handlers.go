c24d863c4d27bd7db1b555ca6cc037f37455e71a
sajjad.dolatkhah <sajjad.dolatkhah@partsoftware.com>
2025-08-02 16:05:54 +0330

    testing

--- bot/handlers/handlers.go
+++ bot/handlers/handlers.go
@@ -8,10 +8,10 @@ import (
 
 func HandleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
     if update.Message != nil {
+        log.Printf("RAW UPDATE: %+v\n", update)
         log.Println("Chat ID:", update.Message.Chat.ID)
         log.Println("Message ID:", update.Message.MessageID)
         log.Println("Text:", update.Message.Text)
-        log.Printf("RAW UPDATE: %+v\n", update)
     }
 
     if update.Message.IsCommand() && update.Message.Command() == "start" {
